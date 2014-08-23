77180067dbe222d9cffccd545dc500b718bcc7c4
wannabeCitizen <doorsofskirpan@gmail.com>
2014-08-23 16:43:27 -0600

    small testing fixes

--- manager.py
+++ manager.py
@@ -1,10 +1,8 @@
 from flask import Flask, session, redirect, url_for, escape, request, render_template
 from flask_googlelogin import GoogleLogin
 from flask.ext import restful
-from flask.ext.restful import reqparse
 
-from resources.organization import Organization
-from resources.user import UserEP
+from resources.organization import Organization, AllOrgs, Organization, OrgMember, OrgOwner
 
 from mongoengine import connect
 
@@ -28,8 +26,8 @@ def devIndex():
 #Expected variables are placed in carrot brackets with a type declaration
 api.add_resource(AllOrgs, '/api/org')
 api.add_resource(Organization, '/api/org/<string:org_id>')
-api.add_resource(OrgMember, 'api/org/<string:org_id/member')
-api.add_resource(OrgOwner, 'api/org/<string:org_id/owner')
+api.add_resource(OrgMember, '/api/org/<string:org_id/member')
+api.add_resource(OrgOwner, '/api/org/<string:org_id/owner')
 
 
 """
