4548ffb25d07522e9ade09d524fc0a615066131f
wannabeCitizen <doorsofskirpan@gmail.com>
2014-08-23 23:28:09 -0600

    Login stuff fixed up

--- manager.py
+++ manager.py
@@ -6,7 +6,8 @@ from flask_googlelogin import GoogleLogin
 
 from lib.model import User, MiniUser
 
-from resources.organization import Organization, OrgMember, OrgOwner, AllOrgs
+from resources.organization import (Organization, OrgMember, OrgOwner, AllOrgs,
+                                    Login)
 
 from mongoengine import connect
 
@@ -20,7 +21,7 @@ app.config.update(
     SECRET_KEY='a3l2kfn93+09cn]diosu9fen[Nofo3indcMJdkjJDJ29',
     GOOGLE_LOGIN_CLIENT_ID='666333715586-orteiqqs7eq4h6he61epr2eg1fkm3e55.apps.googleusercontent.com',
     GOOGLE_LOGIN_CLIENT_SECRET='dF6S3MbnyHzP5WG9eBe1u_o0',
-    GOOGLE_LOGIN_REDIRECT_URI='http://localhost:5000/oauth2callback')
+    GOOGLE_LOGIN_REDIRECT_URI='http://127.0.0.1/dev/oauth2callback')
 
 googlelogin = GoogleLogin(app)
 
@@ -30,19 +31,7 @@ connect('projectsystem')
 def load_user(userid):
     return User.objects(google_id=userid).first()
 
-
-@app.route('/oauth2callback')
-@googlelogin.oauth2callback
-def login(token, userinfo, **params):
-    user = User.objects(google_id=userinfo['id']).first()
-    if not user:
-        current_time = datetime.datetime.now()
-        user = User(google_id=userinfo['id'], email=userinfo['email'], name=userinfo['name'], joined_on=current_time )
-        user.minified = MiniUser(google_id=userinfo['id'], email=userinfo['email'], name=userinfo['name'])
-        user.save()
-
-    login_user(user)
-    return render_template('devIndex.html', json.loads(user.to_json()))   
+  
 
 @app.route('/logout')
 def logout():
@@ -65,6 +54,7 @@ api.add_resource(AllOrgs, '/api/org')
 api.add_resource(Organization, '/api/org/<string:org_id>')
 api.add_resource(OrgMember, '/api/org/<string:org_id>/member')
 api.add_resource(OrgOwner, '/api/org/<string:org_id>/owner')
+api.add_resource(Login, 'api/login')
 
 
 
--- resources/user.py
+++ resources/user.py
@@ -2,8 +2,14 @@ from flask import request
 
 from flask.ext import restful
 from flask.ext.restful import fields, marshal_with, reqparse
+from flask_login import login_user, make_secure_token
 
 from lib.db_utils import create_user, get_user, delete_user, update_user
+from lib.model import User, MiniUser
+
+from manager import googlelogin
+
+import json
 
 post_parser = reqparse.RequestParser()
 post_parser.add_argument(
@@ -29,6 +35,33 @@ update_parser.add_argument(
     help='Figure out if its an add or removal',
 )
 
+class Login(restful.Resource):
+
+    def post(self):
+        #Grab code from authorization
+        data = request.get_json()
+        code = data['code']
+
+        #Get redirect uri for next request
+        redirect uri = googlelogin.redirect_uri
+
+        #Make token request
+        token, userinfo = googlelogin.login(code, redirect_uri)
+
+        #Check for user in DB
+        user = User.objects(google_id=userinfo['id']).first()
+        
+        #If we don't know you, we add your ass
+        if not user:
+            current_time = datetime.datetime.now()
+            user = User(google_id=userinfo['id'], email=userinfo['email'], name=userinfo['name'], joined_on=current_time )
+            user.minified = MiniUser(google_id=userinfo['id'], email=userinfo['email'], name=userinfo['name'])
+            user.save()
+
+        #Add user to the flask-login
+        login_user(user)        
+
+        return json.loads(user.to_json())
 
 
 class User(restful.Resource):
