b326a550bbb904da2619ba3a41b7f62f46fbd0e1
wannabeCitizen <doorsofskirpan@gmail.com>
2014-09-30 20:46:44 -0600

    started the project resource

--- static/js/api.svc.js
+++ static/js/api.svc.js
@@ -28,5 +28,34 @@ define([], function () {
         });
     }];
 
+    factory.ProjectApi = ['$resource', function ($resource) {
+        return $resource('/api/org/:orgId/project/:projectId', {orgId: '@orgId', projectId: '@unique'}, {
+            update: { method: 'PUT'},
+            addMember: { method: 'PUT', },
+            delMember: { method: 'DELETE', },
+            addFollower: { method: 'PUT',},
+            delFollower: { method: 'DELETE',},
+            addRole: { method: 'POST', },
+            delRole: { method: 'DELETE',},
+            updateRole: { method: 'PUT',},
+            addTask: { method: 'POST',},
+            delTask: { method: 'DELETE',},
+            updateTask: { method: 'PUT',},
+            addVote: { method: 'POST',},
+            delVote: { method: 'DELETE',},
+            updateRole: { method: 'PUT',},
+            castBallot: { method: 'PUT', },
+            addPhase: { method: 'POST',},
+            delPhase: { method: 'DELETE',},
+            updatePhase: { method: 'PUT',},
+            addComment: { method: 'POST',},
+            delComment: { method: 'DELETE',},
+            updateComment: { method: 'PUT',},
+            addReply: { method: 'POST',},
+            delReply: { method: 'DELETE',},
+            updateReply: { method: 'PUT',},
+        });
+    }];
+
     return factory;
 });
