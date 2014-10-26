f609ece7862277ed1394f44124dae2cee82e396b
Justin Murray <murrayju@addpcs.com>
2014-10-26 16:03:00 -0400

    New google auth stuff

--- static/js/user.svc.js
+++ static/js/user.svc.js
@@ -34,25 +34,27 @@ define(['angular', 'gapi'], function (angular, gapi) {
         $rootScope.$on('event:google-plus-signin-success', function (event, authResult) {
             // Send login to server or save into cookie
             $log.log('user authenticated with google', authResult);
+            svc.currentUser = {};
             svc.currentUser.googleAuth = authResult;
-            var api = $http.post('/api/login', authResult).then(function (response) {
-                $log.log('login api success', response.data);
-                svc.currentUser.api = response.data;
-            }, function (err) {
-                $log.error('login api fail', err);
-                svc.currentUser.api = null;
-            });
 
             var plus = $q.defer();
             gapi.client.load('plus', 'v1', function () {
                 gapi.client.plus.people.get({userId: 'me'}).execute(function (resp) {
-                    $log.log('google plus api', resp);
-                    svc.currentUser.googlePlus = resp;
                     return resp.error ? plus.reject(resp.error) : plus.resolve(resp);
                 });
             });
 
-            $q.all([api, plus.promise]).finally(function () {
+            plus.promise.then(function (plusData) {
+                svc.currentUser.googlePlus = plusData;
+
+                return $http.post('/api/login', svc.currentUser).then(function (response) {
+                    $log.log('login api success', response.data);
+                    svc.currentUser.api = response.data;
+                }, function (err) {
+                    $log.error('login api fail', err);
+                    svc.currentUser.api = null;
+                });
+            }).then(function () {
                 svc.currentUser.loggedIn = true;
 
                 svc.currentUser.name = (svc.currentUser.googlePlus && svc.currentUser.googlePlus.displayName) ||
@@ -67,6 +69,9 @@ define(['angular', 'gapi'], function (angular, gapi) {
                 svc.currentUser.logout = function () {
                     gapi.auth.signOut();
                 };
+            }, function (err) {
+                $log.error('login failed', err);
+                gapi.auth.signOut();
             });
         });
 
