c02c4741b7091c2b626e1cf6238aefe39b781c74
Justin Murray <murrayju@addpcs.com>
2014-11-26 17:59:52 -0500

    Docker stuff

---
+++ proxy.js
@@ -0,0 +1,6 @@
+var http = require('http'),
+    httpProxy = require('http-proxy');
+//
+// Create your proxy server and set the target in the options.
+//
+httpProxy.createProxyServer({target:'http://192.168.59.103:5000'}).listen(80);
